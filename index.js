6a810ff56d69ba32dd68f055228f904eba99982b
celeste.tech@icloud.com <celeste.tech@icloud.com>
2021-04-06 13:57:31 -0500

    commit tree b579bb5848e5d83af670dc31e071e850718b3666

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Celeste Carter";
+const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const altWelcomeMessage = welcomeMessage.toLowerCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
